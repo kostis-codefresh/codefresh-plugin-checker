46f7d0d67a75720119a0906afdeb57ee4591b63c
Kostis Kapelonis <kostis@codefresh.io>
2020-01-02 23:32:58 +0200

    Initial parsing

--- go.mod
+++ go.mod
@@ -3,8 +3,7 @@ module github.com/kostis-codefresh/codefresh-plugin-checker
 go 1.13
 
 require (
-	github.com/docker/distribution v2.7.1+incompatible
-	github.com/docker/libtrust v0.0.0-20160708172513-aabc10ec26b7
+	github.com/docker/distribution v2.7.1+incompatible // indirect
 	github.com/heroku/docker-registry-client v0.0.0-20190909225348-afc9e1acc3d5
 	github.com/opencontainers/image-spec v1.0.1 // indirect
 )
--- validate-step-images.go
+++ validate-step-images.go
@@ -35,10 +35,15 @@ type StepDetails struct {
 	ImagesUsed []DockerImageName
 }
 
+type ParsingContext struct {
+	nestingLevel  int
+	currentStep   *StepDetails
+	finishedSteps []StepDetails
+}
+
 func main() {
 	fmt.Println("Checking node image")
 
-	// url      := "https://registry-1.docker.io/"
 	url := "https://registry.hub.docker.com"
 	username := "" // anonymous
 	password := "" // anonymous
@@ -79,31 +84,87 @@ func readJson() {
 	json.Unmarshal(jsonData, &v)
 
 	data := v.([]interface{})
+	p := new(ParsingContext)
+	p.nestingLevel = 0
 
 	for _, stepDef := range data {
 		stepContent := stepDef.(map[string]interface{})
 
-		for k, v := range stepContent {
-			switch v := v.(type) {
-			case string:
-				fmt.Println(k, v, "(Found string)")
-			case float64:
-				fmt.Println(k, v, "(Found float64)")
-			case []interface{}:
-				fmt.Println(k, "(Found array):")
-				for i, u := range v {
-					fmt.Println("    ", i, u)
-				}
-			case map[string]interface{}:
-				fmt.Println(k, "(Found object): ")
-				// for i, u := range v {
-				// 	fmt.Println("    ", i, u)
-				// }	
-			default:
-				fmt.Println(k, v, "(Found unknown)")
-			}
+		visitMap(stepContent, p)
+
+	}
+
+}
+
+func visitMap(stepContent map[string]interface{}, p *ParsingContext) {
+
+	for k, v := range stepContent {
+		fmt.Printf("Inside %s at %d\n", k, p.nestingLevel)
+
+		switch v := v.(type) {
+		case string:
+			fmt.Println(k, v, "(Found string)")
+			storeStepInfo(k, v, p)
+		case float64:
+			fmt.Println(k, v, "(Found float64)")
+		case []interface{}:
+			fmt.Println(k, "(Found array):")
+			// for i, u := range v {
+			// 	fmt.Println("    ", i, u)
+			// }
+		case map[string]interface{}:
+			fmt.Println(k, "(Found object): ")
+			p.nestingLevel++
+			visitMap(v, p)
+			p.nestingLevel--
+		default:
+			fmt.Println(k, v, "(Found unknown)")
 		}
 
 	}
 
+}
+
+func storeStepInfo(key string, value string, p *ParsingContext) {
+	switch key {
+	case "image":
+		storeImageInfo(value, p)
+	case "version":
+		if p.nestingLevel != 0 {
+			return
+		}
+		storeVersionInfo(value, p)
+	case "sources":
+		if p.nestingLevel != 1 {
+			return
+		}
+		storeSourcesInfo(value, p)
+	case "name":
+		if p.nestingLevel != 1 {
+			return
+		}
+		storeNameInfo(value, p)
+	}
+}
+
+func storeImageInfo(fullDockerImage string, p *ParsingContext) {
+	var dockerImage DockerImageName
+	dockerImage.BaseImage = fullDockerImage
+	p.currentStep.ImagesUsed = append(p.currentStep.ImagesUsed, dockerImage)
+}
+
+func storeVersionInfo(pluginVersion string, p *ParsingContext) {
+	if p.currentStep != nil {
+		p.finishedSteps = append(p.finishedSteps, *p.currentStep)
+	}
+	p.currentStep = new(StepDetails)
+	p.currentStep.Version = pluginVersion
+}
+
+func storeSourcesInfo(pluginSources string, p *ParsingContext) {
+	p.currentStep.SourceURL = pluginSources
+}
+
+func storeNameInfo(pluginName string, p *ParsingContext) {
+	p.currentStep.Name = pluginName
 }
