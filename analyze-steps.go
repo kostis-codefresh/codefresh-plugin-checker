e30e7f1a0c48ded97efa52482e16a51d27af772f
Kostis Kapelonis <kostis@codefresh.io>
2020-02-21 20:21:59 +0200

    Split code into two files

---
+++ analyze-steps.go
@@ -0,0 +1,159 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+	"strings"
+)
+
+type StepStatus int
+
+const (
+	Ok StepStatus = iota + 1
+	NotOk
+	Incomplete
+)
+
+type DockerImageName struct {
+	BaseImage string
+	Tag       string
+	HasTag    bool
+}
+type StepDetails struct {
+	Name       string
+	Version    string
+	SourceURL  string
+	Status     StepStatus
+	ImagesUsed []DockerImageName
+}
+
+type ParsingContext struct {
+	nestingLevel  int
+	currentStep   *StepDetails
+	finishedSteps []StepDetails
+}
+
+func readJSON(fileName string) []StepDetails {
+
+	jsonFile, err := os.Open(fileName)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer jsonFile.Close()
+
+	jsonData, err := ioutil.ReadAll(jsonFile)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var v interface{}
+	json.Unmarshal(jsonData, &v)
+
+	data := v.([]interface{})
+	p := new(ParsingContext)
+	p.nestingLevel = 0
+
+	for _, stepDef := range data {
+		stepContent := stepDef.(map[string]interface{})
+
+		readSingleStep(stepContent, p)
+
+	}
+
+	return p.finishedSteps
+
+}
+
+func readSingleStep(stepContent map[string]interface{}, p *ParsingContext) {
+	p.currentStep = new(StepDetails)
+	p.nestingLevel = 0
+
+	visitMap(stepContent, p)
+
+	p.finishedSteps = append(p.finishedSteps, *p.currentStep)
+	fmt.Println(">>>>>>>>>>>>>Finished with new step ", p.currentStep.Name)
+
+}
+
+func visitMap(stepContent map[string]interface{}, p *ParsingContext) {
+
+	for k, v := range stepContent {
+		// fmt.Printf("Inside %s at %d\n", k, p.nestingLevel)
+
+		switch v := v.(type) {
+		case string:
+			fmt.Println(k, v, "(Found string at )", p.nestingLevel)
+			storeStepInfo(k, v, p)
+		case []interface{}:
+			fmt.Println(k, "(Found array):")
+			storeSourcesInfo(k, v, p)
+			// for i, u := range v {
+			// 	fmt.Println("    ", i, u)
+			// }
+		case map[string]interface{}:
+			fmt.Println(k, "(Found object): ")
+			p.nestingLevel++
+			visitMap(v, p)
+			p.nestingLevel--
+		default:
+			fmt.Println(k, v, "(Found unknown)")
+		}
+
+	}
+
+}
+func storeSourcesInfo(key string, values []interface{}, p *ParsingContext) {
+	if p.nestingLevel != 1 || key != "sources" || len(values) == 0 {
+		return
+	}
+	p.currentStep.SourceURL = values[0].(string)
+}
+
+func storeStepInfo(key string, value string, p *ParsingContext) {
+	switch key {
+	case "image":
+		storeImageInfo(value, p)
+
+	case "version":
+		if p.nestingLevel == 1 {
+			storeVersionInfo(value, p)
+		}
+
+	case "name":
+		if p.nestingLevel != 1 {
+			return
+		}
+		storeNameInfo(value, p)
+	}
+}
+
+func storeImageInfo(fullDockerImage string, p *ParsingContext) {
+	var dockerImage DockerImageName
+
+	if strings.Contains(fullDockerImage, ":") {
+		imageAndTag := strings.Split(fullDockerImage, ":")
+		dockerImage.BaseImage = imageAndTag[0]
+		dockerImage.HasTag = true
+		dockerImage.Tag = imageAndTag[1]
+
+	} else {
+		dockerImage.HasTag = false
+		dockerImage.BaseImage = fullDockerImage
+	}
+	p.currentStep.ImagesUsed = append(p.currentStep.ImagesUsed, dockerImage)
+}
+
+func storeVersionInfo(pluginVersion string, p *ParsingContext) {
+	p.currentStep.Version = pluginVersion
+}
+
+func storeNameInfo(pluginName string, p *ParsingContext) {
+	p.currentStep.Name = pluginName
+	fmt.Println(">>>>>>>>>>>>>Found step ", pluginName)
+
+}
--- go.mod
+++ go.mod
@@ -4,8 +4,8 @@ go 1.13
 
 require (
 	github.com/docker/distribution v2.7.1+incompatible // indirect
-	github.com/heroku/docker-registry-client v0.0.0-20190909225348-afc9e1acc3d5
-	github.com/lib/pq v1.3.0
+	github.com/heroku/docker-registry-client v0.0.0-20190909225348-afc9e1acc3d5 // indirect
+	github.com/lib/pq v1.3.0 // indirect
 	github.com/opencontainers/image-spec v1.0.1 // indirect
 	github.com/stretchr/testify v1.4.0
 )
